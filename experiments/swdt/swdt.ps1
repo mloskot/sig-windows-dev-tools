11da7b3f7aeb96e4b0ae9c02e6dd74308e4bd858
Mateusz Łoskot <mateusz@loskot.net>
2024-01-10 19:14:48 +0100

    feat: Add convenience go run runners for SWDT CLI
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

---
+++ experiments/swdt/swdt.ps1
@@ -0,0 +1,26 @@
+Set-StrictMode -Version Latest
+
+Write-Host "[swdt.ps1] $args"
+
+if (-not (Test-Path -Path (Join-Path -Path (Get-Location) -ChildPath "go.work"))) {
+  & go work init .
+  Push-Location .
+  & go mod tidy
+  Pop-Location
+}
+
+$startAt = (Get-Date)
+if ($args[0] -eq "test") {
+  & go test -v ./...
+} else {
+  & go run -buildvcs=true ./main.go $args | Out-Default
+}
+$runTime = (Get-Date) - $startAt
+
+if ($global:LASTEXITCODE -gt 0) {
+  $success = "non-success"
+} else {
+  $success = "success"
+}
+Write-Host ("[swdt.ps1] Run in {0:00}:{1:00}:{2:00} and exited with {3} code" `
+  -f $runTime.Hours, $runTime.Minutes, $runTime.Seconds, $success)
